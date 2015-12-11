9cb2919fbfce55cad13b928d06178cd254ba3048
mposa <michael.a.posa@gmail.com>
2015-12-11 00:56:57 -0500

    Added faster null space generation.

--- util/NullQR.m
+++ util/NullQR.m
@@ -3,7 +3,14 @@ function [nullA,rangeAt] = NullQR(A,eps)
     if ~exist('eps','var')
         eps = 10^-8;
     end
-
+    
+    if exist('spspaces','file') == 2
+      % from http://fr.mathworks.com/matlabcentral/fileexchange/11120-null-space-of-a-sparse-matrix
+      [spleft] = spspaces(A',1,1e-12);
+      nullA=spleft{1}(spleft{3},:)';
+      rangeAt=spleft{4}(:,spleft{2});
+    end
+    
     n = size(A,1);
     [q,r,e] = qr(A');
     [e,~] = find(e);
@@ -24,7 +31,7 @@ function [nullA,rangeAt] = NullQR(A,eps)
     nullA = q(:,indxNull);
     Atperm = A(e,:)';
     rangeAt = Atperm(:,setdiff(1:n,indxNull));
-   
+   keyboard
 
 
 
